05006f304e5dee1c8b9fd755319e24969725d50e
ndoelger <doelgern@gmail.com>
2023-07-28 15:42:13 -0400

    adding console log

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
 
-require('dotenv').config();
+require("dotenv").config();
 // Connect to the database
-require('./config/database');
+require("./config/database");
 
 // const indexRouter = require('./routes/api/index');
 // const usersRouter = require('./routes/api/users');
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 
 //------------------------------------------------
 //importing tools needed for api
 //------------------------------------------------
-var cors = require('cors');
-var inventoryRouter = require('./routes/api/inventoryitems')
+var cors = require("cors");
+var inventoryRouter = require("./routes/api/inventoryitems");
 //------------------------------------------------
 
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(require("./config/checkToken.js"));
 
 //--------------------------------------
@@ -44,31 +43,32 @@ app.use(cors());
 const port = process.env.PORT || 3001;
 
 // Put API routes here, before the "catch all" route
-app.use('/api/users', require('./routes/api/users'));
-app.use('/inventoryitems', inventoryRouter);
+console.log(2);
+app.use("/api/users", require("./routes/api/users"));
+app.use("/inventoryitems", inventoryRouter);
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX/API requests
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Express app running on port ${port}`);
 });
 
