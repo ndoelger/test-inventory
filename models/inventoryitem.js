8642f430520f66a081d140358b13920ca672220a
ndoelger <doelgern@gmail.com>
2023-07-28 16:55:41 -0400

    test

--- models/inventoryitem.js
+++ models/inventoryitem.js
@@ -4,7 +4,7 @@ const inventorySchema = new mongoose.Schema({
   productname: {type: String, required: true},
   quantity: {type: Number, required: true},
   SKU: {type: String, required: true},
-  user: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true}
+  user: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: false}
 },{collection: 'inventoryitems'})
 
 module.exports = mongoose.model('inventory', inventorySchema);
