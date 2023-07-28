730e910b23e0800e4ce9249cb889afc53723b38b
ndoelger <doelgern@gmail.com>
2023-07-28 16:15:35 -0400

    put server inside

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,12 +1,12 @@
 import sendRequest from "./send-request";
-import { API } from "../../server";
+const BASE_URL = '/api/users';
 
 export async function signUp(userData) {
-  return sendRequest(BASE_URL, "POST", userData);
+  return sendRequest(BASE_URL, 'POST', userData);
 }
 
 export async function login(credentials) {
-  return sendRequest(`${API}/api/users/login`, "POST", credentials);
+  return sendRequest(`${BASE_URL}/login`, 'POST', credentials);
 }
 
 export async function checkToken() {
